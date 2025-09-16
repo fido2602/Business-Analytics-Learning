80604d0262edb7ab573491653abe85945804ff3f
riya <rio.doingherownthing@gmail.com>
2025-09-16 15:12:11 -0400

    Update Day 1 R practice script and add console screenshot

--- Week 1/R/day1_Rpractice.R
+++ Week 1/R/day1_Rpractice.R
@@ -1,25 +1,50 @@
+
+# --------------------------
+# Day 1 - R  Practice
+# --------------------------
+
+# Test messages
 print("Hello Pace University")
 print("Hello R")
-x<-5
-y<-10
-z<-x+y
-print (z)
-name<- "riya"
-is_student<- TRUE
-pi_value<- 3.14
-scores<-c(20,30,40,50,60)
+
+# Variables and math
+x <- 5
+y <- 10
+z <- x + y
+print(z)
+
+# Character and logical variables
+name <- "riya"
+is_student <- TRUE
+pi_value <- 3.14
+
+# Vectors
+scores <- c(20, 30, 40, 50, 60)
 print(scores)
+
+# Vector indexing
 scores[1]
 scores[3]
 scores[5]
-students <-read.csv("C:/Users/riodo/OneDrive/Desktop/students.csv")
+
+# Vector slicing
 scores[c(2,3)]
 scores[1:3]
 scores[-1]
 scores[-c(2,3)]
-scores[scores>90]
+
+# Conditional filtering
+scores[scores > 90]
+
+# Import CSV (adjust file path as needed)
+students <- read.csv("C:/Users/riodo/OneDrive/Desktop/students.csv")
+
+# Explore dataset
 head(students)
 summary(students)
 str(students)
-install.packages("ggplot2")
+
+# Install + load ggplot2 (only run install once)
+# install.packages("ggplot2")
 library(ggplot2)
+
